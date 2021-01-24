3ca5918394071650d687c055f9da18a2854046d5
belis044 <belis044@umn.edu>
2021-01-23 21:42:22 -0600

    added xml parsing

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 from tkinter import * 
+from sinbad import *
+
+ds = DataSource.connect("http://www.fueleconomy.gov/ws/rest/vehicle/menu/options", format="xml")
+ds.set_param("year", "2011")
+ds.set_param("make", "Toyota").set_param("model", "Camry")
+ds.load()
+try:
+    print(ds.fetch()[0]['value'])
+except Exception as e:
+    print("Couldn't retrieve data.")
+
+
+
+
+
+
+
 window = Tk()
 window.geometry("960x540")
 window.title("Car Emission Tracker")
