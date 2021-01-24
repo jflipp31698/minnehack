df7362ef1d958de788a2da877bed747518740bb2
jflipp31698 <48108288+jflipp31698@users.noreply.github.com>
2021-01-23 20:32:37 -0600

    Basic App

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Carter is stoopid!")
+from tkinter import * 
+window = Tk() 
+
+window.geometry("960x540")
+window.title("Car Emission Tracker")
+window.resizable(False, False)
+window.mainloop() 
